fc416389fec38ac9963cc593e17499580a014500
yuyeol3 <zerom50@gmail.com>
2024-08-31 21:56:41 +0900

    검색기능 개선

--- scripts/search.js
+++ scripts/search.js
@@ -1,46 +1,63 @@
-import { search } from "./api";
-import { convertDotNotationToPath, formatPathToDotNotation, getBasePathFromHash } from "./utils";
+import { getBasePathFromHash, convertDotNotationToPath } from "./utils.js";
+import { search } from "./api.js";
 
 export function setupSearch() {
-	const searchButton = document.getElementById("search-btn");
-	const searchBar = document.getElementById("search-bar");
-	const searchedList = document.getElementById("searched-list");
+    const searchButton = document.getElementById("search-btn");
+    const searchBar = document.getElementById("search-bar");
+    const searchList = document.getElementById("search-list");
 
-	async function handleSearch() {
-		const searchQuery = searchBar.value;
-		if (searchQuery !== "") {
-			location.hash = "w/" + searchQuery;
-			searchBar.value = "";
-		}
-	}
+	searchBar.onfocus = () => {searchList.style.display = "block";}
+	searchBar.onblur = () => {setTimeout(()=>{ searchList.style.display = "none";}, 200);}
 
-	let delayTimer = null;
-	async function updateSearchedList(e) {
-		if (this.value === "")
-			return;
-		// console.log(e.key);
+    async function handleSearch(e, title) {
+        if (searchBar.value === "")
+            return;
+        
+		let target;
+		if (title === undefined)
+			target = searchBar.value;
+		else
+			target = title;
 
-		if (e.key == "ArrowUp" || e.key == "ArrowDown")
-			return;
-			
-		if (delayTimer !== null)
-			clearTimeout(delayTimer);
+		searchList.innerHTML = "";
+		searchBar.value = "";
+		searchBar.blur();
 
-		delayTimer = setTimeout(async ()=> {
-			const response = await search( getBasePathFromHash(searchBar.value) );
+		location.hash = "w/" + target;
+    }
 
-			searchedList.innerHTML = "";
-			for (const element of response.content) {
-				const opt = document.createElement("option");
-				opt.value = convertDotNotationToPath( element );
-				searchedList.appendChild(opt);
-			}
-		}, 500);
+    let delayTimer = null;
+
+    async function updateSearchedList(e) {
+        // Ensure that the update happens only after IME composition has finished
+        let target = searchBar.value;
+
+		searchBar.value = target;
+        if (target === "")
+            return;
+
+        if (e.key == "ArrowUp" || e.key == "ArrowDown")
+            return;
+            
+        if (delayTimer !== null)
+            clearTimeout(delayTimer);
+
+        delayTimer = setTimeout(async ()=> {
+            const response = await search( getBasePathFromHash(target) );
+
+            searchList.innerHTML = "";
+            for (const element of response.content) {
+                const opt = document.createElement("div");
+                opt.innerText = convertDotNotationToPath( element );
+				opt.classList.add("searchopt")
+				opt.onclick = ()=> {handleSearch(undefined, element);}
+                searchList.appendChild(opt);
+            }
+        }, 750);
+    }
 
 
-	}
-
-	searchButton.onclick = handleSearch;
-	searchBar.onchange = handleSearch;
-	searchBar.onkeyup = updateSearchedList;
+    searchButton.onclick = handleSearch;
+    // searchBar.onchange = handleSearch;
+    searchBar.onkeyup = updateSearchedList;
 }
